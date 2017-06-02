d8c081b85333a19debcf2c589ff1a41f81d868ac
TravisWay <travis_sti@yahoo.com>
2017-06-02 14:01:30 -0600

    June 2 work

---
+++ Java/workspace/Java1-Chapter04/labs/Overflow.java
@@ -0,0 +1,16 @@
+package labs;
+
+public class Overflow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int big = 2147483647;
+		int bigger = big + 1;
+		
+		System.out.println(big + " " + bigger);
+
+	}
+
+}
