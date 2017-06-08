205e026fc670ee9a94c1057285f2bccaa1e6bb65
TravisWay <travis_sti@yahoo.com>
2017-06-08 09:54:45 -0600

    June 8th labs

---
+++ Java/workspace/Java1-Chapter09/labs/Giraffe.java
@@ -0,0 +1,20 @@
+package labs;
+
+public class Giraffe extends Animal {
+
+	
+
+	@Override
+	void makeNoise() {
+		// TODO Auto-generated method stub
+		System.out.println("I'm Tall");
+	}
+
+	public void EatLeaves() {
+		System.out.println("Just eating some leaves here.");
+		
+		
+	}
+	
+	
+}
---
+++ Java/workspace/Java1-Chapter09/labs/Lion.java
@@ -0,0 +1,15 @@
+package labs;
+
+public class Lion extends Animal{
+
+	public void makeNoise() {
+		System.out.println("ROAR!");
+		
+	}
+	public void EatGiraffe(){
+		
+		System.out.println("Careful......Just do it");
+		
+	}
+	
+}
